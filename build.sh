17303a58cf0ab97ca8f2864f64fde3efeff7b1d5
Tinic Uro <tinicuro@gmail.com>
2021-03-29 11:03:35 -0700

    Fix build.

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ cmake --build .
 cd ..
 mkdir -p pendant2021_bootloaded_dgb
 cd pendant2021_bootloaded_dgb
-cmake -G "$build_type" -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -G "$build_type" -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DBOOTLOADED=1 -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build .
 cd ..
 mkdir -p pendant2021_bootloader_rel
@@ -20,6 +20,6 @@ cmake --build .
 cd ..
 mkdir -p pendant2021_bootloaded_rel
 cd pendant2021_bootloaded_rel
-cmake -G "$build_type" -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -G "$build_type" -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DBOOTLOADED=1 -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
 cd ..
